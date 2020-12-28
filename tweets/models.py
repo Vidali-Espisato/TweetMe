2ce03a25de8d796e5650e144d6a77d162534bc3b
Manish Roy <manish.11612939@lpu.in>
2020-12-28 21:39:05 +0530

    Removed video in the repo

--- tweets/models.py
+++ tweets/models.py
@@ -1,11 +1,24 @@
+import random
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
-
+User = settings.AUTH_USER_MODEL
 
 class Tweet(models.Model):
-    content = models.CharField(max_length=500, blank=True, null=True)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    content = models.CharField(max_length=240, blank=True, null=True)
     image = models.FileField(upload_to="images/", blank=True, null=True)
+
+    class Meta:
+        ordering = ['-pk']
 
     def __str__(self):
         return self.content
+
+    def serialize(self):
+        return {
+            'id': self.pk,
+            'content': self.content,
+            'likes': random.randint(0, 299)
+        }
