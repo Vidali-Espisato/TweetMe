8a6231ab806d191f880ed8c2379cfeab6fa25c77
Manish Roy <manish.11612939@lpu.in>
2020-12-30 21:09:26 +0530

    REST API views with permission classes

--- tweetme/settings.py
+++ tweetme/settings.py
@@ -122,3 +122,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 CORS_ORIGIN_ALLOW_ALL = True
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication'
+    ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        # 'rest_framework.renderers.BrowsableAPIRenderer'
+    ]
+}
--- tweets/views.py
+++ tweets/views.py
@@ -3,6 +3,10 @@ from django.conf import settings
 from django.utils.http import is_safe_url
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404, JsonResponse
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from .forms import TweetForm
 from .serializers import TweetSerializer
 from .models import Tweet
@@ -14,54 +18,79 @@ ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 def home_view(request, *args, **kwargs):
     return render(request, 'pages/home.html')
 
+@api_view(['POST'])
+@authentication_classes([SessionAuthentication])
+@permission_classes([IsAuthenticated])
 def tweet_create_view(request, *args, **kwargs):
-    serializer = TweetSerializer(data=request.POST or None)
-    if serializer.is_valid():
+    serializer = TweetSerializer(data=request.POST)
+    if serializer.is_valid(raise_exception=True):
         serializer.save(user=request.user)
-        return JsonResponse(serializer.data, status=201)
-    return JsonResponse(serializer.errors, status=400)
+        return Response(serializer.data)
+    return Response(serializer.errors, status=400)
 
-
-def tweet_create_view_just_django(request, *args, **kwargs):
-    user = request.user
-    if not user.is_authenticated:
-        user = None
-        if request.is_ajax():
-            return JsonResponse({}, status=401)
-        return redirect(settings.LOGIN_URL)
-    form = TweetForm(request.POST or None)
-    next_url = request.POST.get("next") or None
-    if form.is_valid():
-        obj = form.save(commit=False)
-        obj.save()
-        if request.is_ajax():
-            return JsonResponse(obj.serialize(), status=201)
-        if next_url != None and is_safe_url(next_url, allowed_hosts=ALLOWED_HOSTS):
-            return redirect(next_url)
-        form = TweetForm()
-    if form.errors and request.is_ajax():
-        return JsonResponse(form.errors, status=400)
-    return render(request, 'components/form.html', context={'form': form})
-
-
+@api_view(['GET'])
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
-    # tweets_list = [{'id': x.id, 'content': x.content,
-    #                 'likes': random.randint(0, 99)} for x in qs]
-    tweets_list = [x.serialize() for x in qs]
-    data = {'isUser': False, 'response': tweets_list}
-    return JsonResponse(data, status=200)
+    serializer = TweetSerializer(qs, many=True)
+    return Response(serializer.data)
 
-
+@api_view(['GET'])
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
-    data = {
-        'id': tweet_id,
-    }
-    status = 200
-    try:
-        tweet = Tweet.objects.get(id=tweet_id)
-        data['content'] = tweet.content
-    except:
-        data['error'] = "Not Found!"
-        status = 404
-    return JsonResponse(data, status=status)
+    qs = Tweet.objects.filter(id=tweet_id)
+    if not qs.exists():
+        return Response({}, status=404)
+    serializer = TweetSerializer(qs.first())
+    return Response(serializer.data)
+
+
+# def tweet_create_view(request, *args, **kwargs):
+#     serializer = TweetSerializer(data=request.POST or None)
+#     if serializer.is_valid():
+#         serializer.save(user=request.user)
+#         return JsonResponse(serializer.data, status=201)
+#     return JsonResponse({}, status=400)
+
+
+# def tweet_create_view(request, *args, **kwargs):
+#     user = request.user
+#     if not user.is_authenticated:
+#         user = None
+#         if request.is_ajax():
+#             return JsonResponse({}, status=403)
+#         return redirect(settings.LOGIN_URL)
+#     form = TweetForm(request.POST or None)
+#     next_url = request.POST.get("next") or None
+#     if form.is_valid():
+#         obj = form.save(commit=False)
+#         obj.save()
+#         if request.is_ajax():
+#             return JsonResponse(obj.serialize(), status=201)
+#         if next_url != None and is_safe_url(next_url, allowed_hosts=ALLOWED_HOSTS):
+#             return redirect(next_url)
+#         form = TweetForm()
+#     if form.errors and request.is_ajax():
+#         return JsonResponse(form.errors, status=400)
+#     return render(request, 'components/form.html', context={'form': form})
+
+
+# def tweet_list_view(request, *args, **kwargs):
+#     qs = Tweet.objects.all()
+#     # tweets_list = [{'id': x.id, 'content': x.content,
+#     #                 'likes': random.randint(0, 99)} for x in qs]
+#     tweets_list = [x.serialize() for x in qs]
+#     data = {'isUser': False, 'response': tweets_list}
+#     return JsonResponse(data, status=200)
+
+
+# def tweet_detail_view(request, tweet_id, *args, **kwargs):
+#     data = {
+#         'id': tweet_id,
+#     }
+#     status = 200
+#     try:
+#         tweet = Tweet.objects.get(id=tweet_id)
+#         data['content'] = tweet.content
+#     except:
+#         data['error'] = "Not Found!"
+#         status = 404
+#     return JsonResponse(data, status=status)
