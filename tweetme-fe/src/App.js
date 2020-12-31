373428948cd65d861eea7d8d16050e535f7582f1
Manish Roy <manish.11612939@lpu.in>
2021-01-01 02:20:17 +0530

    Basic React setup

---
+++ tweetme-fe/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  const [tweets, setTweets] = useState
+  return (
+  <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
