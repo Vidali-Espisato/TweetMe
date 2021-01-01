bc9b0ce16f7cf0b51bf85edb287cb1b5eeabfc78
Manish Roy <manish.11612939@lpu.in>
2021-01-01 11:37:54 +0530

    React-Django Integration

--- tweetme-fe/src/App.js
+++ tweetme-fe/src/App.js
@@ -1,11 +1,47 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import Tweet from './Tweet';
+
+
+const loadTweets = (callback) => {
+  const xhr = new XMLHttpRequest();
+  const method = "GET";
+  const url = "http://127.0.0.1:8000/api/v1/tweets/";
+  const responseType = "json";
+  xhr.responseType = responseType;
+  xhr.open(method, url);
+  xhr.onload = () => {
+    callback(xhr.response, xhr.status);
+  };
+  xhr.onerror = err => {
+    console.log(err);
+    callback({message: "An error occurred!"}, 400);
+  }
+  xhr.send();
+};
+
 
 function App() {
-  const [tweets, setTweets] = useState
+  const [tweets, setTweets] = useState([]);
+  
+  useEffect(() => {
+    const myCallback = (response, status) => {
+      if (status === 200) {
+        setTweets(response);
+      } else {
+        alert("An error occurred!");
+      }
+    }
+    loadTweets(myCallback);
+  }, [])
+
   return (
-  <div className="App">
-      <header className="App-header">
+    <div className="App">
+      <header className="App-header">          
       </header>
+      {
+        tweets.map((tweet, index) => <Tweet key={index} tweet={tweet} className="my-10 mx-auto bg-light text-success shadow-lg text-center" />)
+      }
     </div>
   );
 }
--- tweetme-fe/src/Tweet.js
+++ tweetme-fe/src/Tweet.js
@@ -1,1 +1,13 @@
-jhhghgh
+import React from 'react';
+
+function Tweet(props) {
+    const {tweet} = props;
+    const className = props.className ? props.className : "my-10 mx-auto bg-light text-dark";
+    return (
+        <div className={className}>
+            <p>{tweet.id}: {tweet.content}</p>
+        </div>
+    )
+}
+
+export default Tweet;
